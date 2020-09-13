9379a7f1798266a0c40d3c16052dd8d187c5d4b1
rokibulsanto <rhsanto360@gmail.com>
2020-09-13 23:54:47 +0600

    burj-al-arab-googleSignIn working done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7eS8s1GEUWVT-jRgTBYMo1ra15YMOcbg",
+    authDomain: "burj-al-arabians.firebaseapp.com",
+    databaseURL: "https://burj-al-arabians.firebaseio.com",
+    projectId: "burj-al-arabians",
+    storageBucket: "burj-al-arabians.appspot.com",
+    messagingSenderId: "880165622525",
+    appId: "1:880165622525:web:d971dbd947e96939d8e494"
+  };
+
+  export default firebaseConfig;
